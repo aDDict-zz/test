d4f366319cecadfe52046b015fa035777e3d2042
robThot <robthot@gmail.com>
2010-12-09 18:25:32 +0100

    finish 1.0

--- model_bookstore/app/models/book_store.rb
+++ model_bookstore/app/models/book_store.rb
@@ -1,25 +1,38 @@
-class BookStore < ActiveRecord::Base
+class BookStore # < ActiveRecord::Base
   def self.search_by_bookStore(params)
     if params[:bookStores] != "" && Schema.class_exists?(params[:bookStores])
+      @resArray = []
       @domains = {
         "Amazon" => "http://www.amazon.com"
       }
       @Request = eval("#{params[:bookStores]}Request").new(params)
-      @doc = @Request.result
-      @linx = []
-      @linx = eval(params[:bookStores]).new(@doc).getLinxToTheProducts
-      i = 0
-      @linx.each do |link|
-        # van ahol csak relativan van megadva az url( valojaban eloszor rossz linkeket is kovettem, de vegul benne hagytam, who knows )
-        !Regexp.new(/#{@domains[params[:bookStores]]}.*/).match(link) ? link = "#{@domains[params[:bookStores]]}#{link}" : ""
-        if i == 1
+      @doc = @Request.result #; return @doc;
+      if @doc != -1
+        @Schema = eval(params[:bookStores]).new(@doc)
+        @linx = []
+        @linx = @Schema.getLinxToTheProducts;
+        @linx.each do |link|
+          # van ahol csak relativan van megadva az url( valojaban eloszor rossz linkeket is kovettem, de vegul benne hagytam, who knows )
+          !Regexp.new(/#{@domains[params[:bookStores]]}.*/).match(link) ? link = "#{@domains[params[:bookStores]]}#{link}" : ""
           @Schema = eval(params[:bookStores]).new(open(link))
+          @resArray.push({
+            "poduct" => link,
+            "img" => @Schema.getImgUrl,
+            "price" => @Schema.getPrice,
+            "title" => @Schema.getTitle,
+            "author" => @Schema.getAuthor,
+            "description" => @Schema.getDescription,
+            "ISBN" => @Schema.getISBN,
+            "publisher" => @Schema.getPublisher,
+            "reviews" => @Schema.reviews
+          })
         end
-        i += 1
+      else
+        -1
       end
-      @Schema.getImgUrl
+      @resArray
     else
-      "Something went wrong<br /><a href='/'>Back</a>"
+      -2
     end
   end
 end
@@ -28,8 +41,12 @@ end
 # kiterjeszti a String osztalyt
 class String
   def removeHtmlGarbage
-    self.gsub(/(<[^<>]+>)/, "")
+    self.gsub(/(<[^<>]+>)/, "").fixBadlatinOne.escapeHtmlEntities.strip
   end
+
+#  def removeSingleTags
+#    self.gsub(/(<[^<>]+\/>)/, "")
+#  end
 
   def removeHtmlContent(element)
     self.gsub(/(<#{element}.*#{element}>)/, "")
@@ -37,6 +54,29 @@ class String
   # Hpricot bug - img.attributes["src"] nem mukodik
   def getImageSource
     self.split('src="')[1].to_s.split('" ')[0]
+  end
+
+  def escapeHtmlEntities
+    HTMLEntities.new.decode(self)
+  end
+
+  def fixBadlatinOne
+    str = self.to_s
+    entities = {
+    '&#x80;'=>'&#x20AC;', '&#x81;'=>'?',        '&#x82;'=>'&#x201A;', '&#x83;'=>'&#x0192;',
+    '&#x84;'=>'&#x201E;', '&#x85;'=>'&#x2026;', '&#x86;'=>'&#x2020;', '&#x87;'=>'&#x2021;',
+    '&#x88;'=>'&#x02C6;', '&#x89;'=>'&#x2030;', '&#x8A;'=>'&#x0160;', '&#x8B;'=>'&#x2039;',
+    '&#x8C;'=>'&#x0152;', '&#x8D;'=>'?',        '&#x8E;'=>'&#x017D;', '&#x8F;'=>'?',
+    '&#x90;'=>'?',        '&#x91;'=>'&#x2018;', '&#x92;'=>'&#x2019;', '&#x93;'=>'&#x201C;',
+    '&#x94;'=>'&#x201D;', '&#x95;'=>'&#x2022;', '&#x96;'=>'&#x2013;', '&#x97;'=>'&#x2014;',
+    '&#x98;'=>'&#x02DC;', '&#x99;'=>'&#x2122;', '&#x9A;'=>'&#x0161;', '&#x9B;'=>'&#x203A;',
+    '&#x9C;'=>'&#x0153;', '&#x9D;'=>'?',        '&#x9E;'=>'&#x017E;', '&#x9F;'=>'&#x0178;'
+    }
+    entities.each do |k, v|
+      sstr = str.gsub(/(#{k})/, v)
+      str = sstr
+    end
+    str
   end
 end
 
@@ -113,27 +153,28 @@ class AmazonRequest < HttpartyRequest
 
   def getFullContent(pmeters)
     content = Hpricot(get)
+    head = content.search("//head").to_s
     # ha szerepel pager az oldalon, akkor a brutto resultsetet tobb requesttel kell elkerni
     pager = content.search("//td[@class='pagn']")
     if pager.size > 0 && content.search("//h1[@id='noResultsTitle']").size == 0
       total = getTotal(content)["total"]
 
       #  a talalatok maximalasa, ad hoc
-      total > 200 ? total = 14 : total
+      total > 10 ? total = 0 : total
 
       currentRes = getTotal(content)["current"]
       currentPage = 2
-      content = content.search("//body").to_s
+      content = content.search("//body").inner_html
       while currentRes <= total do
         @query["page"] = currentPage
-        result = Hpricot(get).search("//body")
+        result = Hpricot(get)
         currentRes = getTotal(result)["current"]
-        content += result.to_s
+        content += result.search("//body").inner_html
         currentPage += 1
       end
-      content
+      "<html>#{head}<body>#{content}</body></html>"
     else
-      content
+      -1
     end
   end
 
--- model_bookstore/app/models/schema.rb
+++ model_bookstore/app/models/schema.rb
@@ -1,26 +1,31 @@
 class Schema
 
-  attr_accessor :doc, :linkToTheProduct, :img, :price, :title, :author
+  attr_accessor :xpaths, :doc, :linkToTheProduct, :img, :price, :title, :author, :description, :ISBN, :publisher, :reviews
 
   def initialize(doc)
+    @xpaths = loadYaml
     @doc = createHpricotInstance(doc)
     @linkToTheProduct = getLinxToTheProducts
     @img = getImgUrl
     @price = getPrice
     @title = getTitle
     @author = getAuthor
-    @xpaths = loadYaml
+    @description = getDescription
+    @ISBN = getISBN
+    @publisher = getPublisher
+    @reviews = getReviews
   end
 
   def loadYaml
-    File.open("config/xpaths.yml") do |y| YAML::load(y) end
+    hash = File.open("config/xpaths.yml") do |y| YAML::load(y) end
+    hash[self.class.to_s]
   end
 
   def createHpricotInstance(doc)
     Hpricot(doc)
   end
 
-  def getLinxToTheProducts(doc)
+  def getLinxToTheProducts
   end
 
   def getImgUrl
@@ -35,7 +40,21 @@ class Schema
   def getAuthor
   end
 
+  def getDescription
+  end
+
   def self.getLinxToTheProducts
+  end
+
+  def getISBN
+    {}
+  end
+
+  def getPublisher
+  end
+
+  def getReviews
+    {}
   end
 
   def self.class_exists?(name)
@@ -51,32 +70,72 @@ end
 class Amazon < Schema
 
   def getLinxToTheProducts
-    path = "//td[@class='dataColumn']"
+    path = @xpaths["linkToTheProduct"]
     elements = []
-    @doc.search(path).each do |a|
+    @doc.search(@xpaths["linkToTheProduct"]).each do |a|
       elements.push(Hpricot(a.to_s).search("//a")[0].attributes["href"])
     end
     elements
   end
 
   def getImgUrl
-    path = "//img[@id='prodImage']"
-    @doc.at(path).to_s.getImageSource
+    @doc.at(@xpaths["img"]).to_s.getImageSource
   end
 
   def getPrice
-    path = "//b[@class='priceLarge']"
-    @doc.at(path).to_s.removeHtmlGarbage
+    @doc.at(@xpaths["price"]).to_s.removeHtmlGarbage
   end
 
   def getTitle
-    path = "//span[@id='btAsinTitle']"
-    @doc.search(path).inner_html.removeHtmlContent("span")
+    @doc.search(@xpaths["title"]).inner_html.removeHtmlContent("span").removeHtmlGarbage
   end
 
   def getAuthor
-    path = "//div[@class='buying']//span//a"
-    @doc.search(path)[0].to_s.removeHtmlGarbage
+    @doc.search(@xpaths["author"])[0].to_s.removeHtmlGarbage
+  end
+
+  def getDescription
+    str = ""
+    @doc.search(@xpaths["description"]).each do |description|
+      str += description.to_s.removeHtmlGarbage
+    end
+    str
+  end
+
+  def getPublisher
+    str = ""
+    @doc.search(@xpaths["publisher"]).each do |publisher|
+      if Regexp.new(/Publisher/).match(publisher.inner_html)
+        str = publisher.inner_html.removeHtmlContent("b").removeHtmlGarbage
+      end
+    end
+    str
+  end
+
+  def getISBN
+    hash = {}
+    @doc.search(@xpaths["ISBN"]).each do |isbn|
+      if Regexp.new(/ISBN-10/).match(isbn.inner_html)
+        hash["ISBN-10"] = isbn.inner_html.removeHtmlContent("b")
+      end
+      if Regexp.new(/ISBN-13/).match(isbn.inner_html)
+        hash["ISBN-13"] = isbn.inner_html.removeHtmlContent("b")
+      end
+    end
+    hash
+  end
+
+  def getReviews
+    hash = {}
+    @doc.search(@xpaths["reviews"]).each do |a|
+      if Regexp.new(/customer reviews/).match(a.inner_html)
+        hash = {
+          "link" => a.attributes["href"],
+          "content" => a.inner_html
+          }
+      end
+    end
+    hash
   end
 
 end
