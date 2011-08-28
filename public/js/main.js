c438e6bd2392cda0383b9870ce1e1ece74adc07b
robThot <robthot@gmail.com>
2011-08-28 11:57:07 +0200

    tracsak at home

--- public/js/main.js
+++ public/js/main.js
@@ -51,46 +51,51 @@
 */
 Ext.define('IEHH', {
 
-  DEPO                : "",
-  init                : function(){
-    navigator.appName.match("Microsoft") != null ? this.setup() : "";
-  },
+	statics: {
+
+		DEPO: "",
+		init: function(){
+			navigator.appName.match("Microsoft") != null ? this.setup() : "";
+		},
   
-  setup               : function(){ alert("ieFok");
+		setup: function(){
   
-    var thisIframe  = document.createElement('<iframe id="thisIframe" style="display:none;" src="about:blank" />'),
-        thisBody    = document.getElementsByTagName("body")[0];
+			var thisIframe  = document.createElement('<iframe id="thisIframe" style="display:none;" src="about:blank" />'),
+				  thisBody    = document.getElementsByTagName("body")[0];
         
-    thisBody.appendChild(thisIframe);
+			document.appendChild(thisIframe);
     
-    Ext.TaskManager.start({
-      run: IEHH.checkIframeContent,
-      interval: 1000
-    });
-  },
+			Ext.TaskManager.start({
+				run: IEHH.checkIframeContent,
+				interval: 1000
+			});
+		},
   
-  changeContent       : function(){
-    var thisIframe    = document.getElementById("thisIframe"),
-        thisIframeDoc = thisIframe.contentWindow.document;
+		changeContent: function(urlPart){
+			var thisIframe    = document.getElementById("thisIframe"),
+					thisIframeDoc = thisIframe.contentWindow.document;
 
-    thisIframeDoc.open();
-    thisIframeDoc.write(urlPart);
-    thisIframeDoc.close();
-    IEHH.DEPO = urlPart;
-  },
+			thisIframeDoc.open();
+			thisIframeDoc.write(urlPart);
+			thisIframeDoc.close();
+			IEHH.DEPO = urlPart;
+		},
   
-  checkIframeContent  : function(){
-    var thisIframe        = document.getElementById("thisIframe"),
-        thisIframContent  = thisIframe.contentWindow.document.body.innerHTML;
+		checkIframeContent: function(){
+			var thisIframe        = document.getElementById("thisIframe"),
+					thisIframContent  = thisIframe.contentWindow.document.body.innerHTML;
 
-    if (window.location.href.match("#") && thisIframContent != "") {
-      var thisArr = window.location.href.split("#"),
-        thisUrlPart = ["#",thisArr[1]].join("");
-      if (thisUrlPart != thisIframContent) {
-        window.location.href = [thisArr[0],thisIframContent].join("");
-      }
-    }
-  }
+			if (window.location.href.match("#") && thisIframContent != "") {
+				var thisArr = window.location.href.split("#"),
+					thisUrlPart = ["#",thisArr[1]].join("");
+				if (thisUrlPart != thisIframContent) {
+					window.location.href = [thisArr[0],thisIframContent].join("");
+				}
+			}
+		},
+    
+    constructor: function() {}
+	}
 },
   function(){}
 );
@@ -103,9 +108,9 @@ Ext.define('$$', {
     order     : "",
     frontPage : "groups",
     
-    init      : function(){
-      /*if($$.ie)
-        IEHH.setup();*/
+    init      : function(){ console.log(Ext.Array);
+      if($$.ie)
+        IEHH.setup();
       
       Ext.TaskManager.start({
         run: $$.getOrder,
@@ -113,12 +118,12 @@ Ext.define('$$', {
       });
     },
   
-    getOrder  : function(){ console.log(IEHH);
+    getOrder  : function(){ console.log( Ext.Array.indexOf($$.orders, "trillili") );
       var matches = window.location.href.match(/(.*)(#)(.*)/);
       if(matches != null){
-        if($$.orders.indexOf(matches[3]) == -1)
+        if(Ext.Array.indexOf($$.orders, matches[3]) == -1){ alert("frontPage");
           window.location.href = [matches[1],"#",$$.frontPage].join("");
-        else {
+        } else {
           if($$.order != matches[3]){
             if($$.ie)
               IEHH.changeContent(["#",matches[3]].join(""));
@@ -135,12 +140,14 @@ Ext.define('$$', {
     doJob     : function(){
       if($$.order != "")
         switch($$.order){
-          case "groups": //alert("groups");
+          case "groups": alert("groups");
           break;
-          case "demog": //alert("demog");
+          case "demog": alert("demog");
           break;
         }
-    }
+    },
+    
+    constructor: function() {}
   }
 
 },
@@ -154,10 +161,10 @@ Ext.define('$$', {
 );
 
 /*
-|\_____|\   |\_____|\   |\_____|\   |\_____|\   |\_____|\   |\_____|\   |\_____|\   |\_____|\   |\_____|\
-|       0\  |       0\  |       0\  |       0\  |       0\  |       0\  |       0\  |       0\  |       0\
-| A____  /  | A____  /  | A____  /  | A____  /  | A____  /  | A____  /  | A____  /  | A____  /  | A____  /
-|/|/ |/\/   |/|/ |/\/   |/|/ |/\/   |/|/ |/\/   |/|/ |/\/   |/|/ |/\/   |/|/ |/\/   |/|/ |/\/   |/|/ |/\/
+  |\_____|\   |\_____|\   |\_____|\   |\_____|\   |\_____|\   |\_____|\   |\_____|\   |\_____|\   |\_____|\
+  |       0\  |       0\  |       0\  |       0\  |       0\  |       0\  |       0\  |       0\  |       0\
+  | A____  /  | A____  /  | A____  /  | A____  /  | A____  /  | A____  /  | A____  /  | A____  /  | A____  /
+  |/|/ |/\/   |/|/ |/\/   |/|/ |/\/   |/|/ |/\/   |/|/ |/\/   |/|/ |/\/   |/|/ |/\/   |/|/ |/\/   |/|/ |/\/
 */
 
 //Ext.application({
