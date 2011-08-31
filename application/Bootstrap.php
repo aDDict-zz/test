65a5a1dd856fbda038cd96e1687ce81cc1601908
robThot <robthot@gmail.com>
2011-08-31 17:25:49 +0200

    auth begin

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -9,11 +9,33 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap {
         'group', new Zend_Controller_Router_Route(
           'group/:stub',
           array(
-            'controller' => 'group',
-            'action' => 'show'
+            'controller'  => 'group',
+            'action'      => 'show'
           )
         )
     );
+    /*$router->addRoute(
+        'login', new Zend_Controller_Router_Route(
+          'process/',
+          array(
+            'controller'  => 'login',
+            'action'      => 'process'
+          )
+        )
+    );*/
+  }
+  
+  protected function _initPaths() {
+    
+    define('WEB_ROOT', "/newStuff");
+    
+    $rootDir = dirname(dirname(__FILE__));
+    define('ROOT_DIR', $rootDir);
+    set_include_path(get_include_path()
+      /*. PATH_SEPARATOR . ROOT_DIR . '/library/'*/
+      . PATH_SEPARATOR . ROOT_DIR . '/application/forms/'
+    );
+    Zend_Loader_Autoloader::getInstance()->setFallbackAutoloader(true);
   }
 }
 
