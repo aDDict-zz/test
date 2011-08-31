73151b89f5e6403f1bce28bfb101ffe57af1af44
robThot <robthot@gmail.com>
2011-08-31 16:20:42 +0200

    group model is finished

--- application/controllers/GroupController.php
+++ application/controllers/GroupController.php
@@ -2,40 +2,39 @@
 
 class GroupController extends Zend_Controller_Action {
 
+    /*$params = $this->getRequest()->getParams();
+    $params = $this->getRequest()->getPost();
+    echo $this->getRequest()->getPost("data");*/
+
     public function init() {
         /* Initialize action controller here */
     }
 
     public function indexAction() {
-        //$params = $this->getRequest()->getParams();
-        //$params = $this->getRequest()->getPost();
-        //echo $this->getRequest()->getPost("data");
         $groups = new Application_Model_Groups();
-        $groups->getAllForIndexGrouping();
+        $ddd = $groups->getAll(); print_r($ddd);
         // disable the rendering of the view
         $this->_helper->viewRenderer->setNoRender(true);
     }
 
     public function addAction() {
-        // action body
+        
     }
 
     public function editAction() {
-        // action body
+        
     }
 
     public function deleteAction() {
-        // action body
+        
     }
 
     public function showAction() {
       $params = $this->getRequest()->getParams();
       $id     = $params["stub"];
       $groups = new Application_Model_Groups();
-      //echo  Zend_Json::encode($groups->fetchAll($id)); 
+      
       echo  Zend_Json::encode($groups->getGroup($id)); 
-      
-      
       // disable the rendering of the view
       $this->_helper->viewRenderer->setNoRender(true);
     }
--- application/models/Groups.php
+++ application/models/Groups.php
@@ -2,39 +2,50 @@
 
 class Application_Model_Groups extends Zend_Db_Table_Abstract {
   
+  /**
+   * Virtual table
+   * Tables: members, multi, multigroup, groups, user
+   * elements grouped by the multi table(if it has no join, called name is 'egyeb csoportok')
+   */
+  
   protected $_name = 'groups';
 
   public function getGroup($id){
-    $id = (int)$id;
-    $row = $this->fetchRow('id = ' . $id);
+    $id   = (int)$id;
+    $row  = $this->fetchRow('id = ' . $id);
     if (!$row) {
         throw new Exception("Could not find row $id");
     }
     return $row->toArray();
   }
-  
-  public function getAllForIndexGrouping(){
-    //$adapter = $this->getAdapter();
-    //print_r( get_class_methods( $this ) );
-    //print_r( $this->getAdapter() );
     
-    //$select = $this->select(); print_r($select);
-    
-    $res = $this->select()
-             ->from( array('g' => 'groups') );
-             
-    $stmt = $res->query();
-    $result = $stmt->fetchAll(); print_r($result);
-    
+  public function getAll(){
+   
+   $result = $this->_db->query("
+    SELECT
+        if( length( g.name ) >0, g.name, g.title ) realname,
+        if( length( multi.name ) >0, multi.name, multi.title ) mrealname,
+        m.membership,
+        m.group_id,
+        g.title,
+        mg.multiid
+        
+      FROM
+        members m, groups g
+        
+      LEFT JOIN
+        multigroup mg ON g.id = mg.groupid
+        
+      LEFT JOIN
+        multi ON multi.id = mg.multiid
+      
+      AND mg.multiid IN
+        (SELECT id FROM multi WHERE index_grouping='yes')
+      
+    WHERE g.id = m.group_id
+    AND m.user_id = '59446'
+   ")->fetchAll(); return $result;
+   
   }
   
-  public function fetchAll(){
-    return array(
-      "elso" => array(
-        "a1" => 1,
-        "a2" => 2
-      ),
-      "masodik" => "2"
-    );
-  }
 }
