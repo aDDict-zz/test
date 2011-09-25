044c8f1edb648bf651bd807747f3d412f30075bc
robThot <robthot@gmail.com>
2011-09-25 21:37:37 +0200

    refact

--- ruby/IO.rb
+++ ruby/IO.rb
@@ -7,11 +7,18 @@
 #  :db   => 'hirek_old'
 #}
 
+#@params = {
+#  :host => 'localhost',
+#  :user => 'root',
+#  :pssw => 'v',
+#  :db   => 'hirekhu'
+#}
+
 @params = {
   :host => 'localhost',
   :user => 'root',
   :pssw => 'v',
-  :db   => 'hirekhu'
+  :db   => 'hirek'
 }
 
 begin
@@ -35,6 +42,8 @@ end
 
 def rssWrite(items, title)
   
+  @title = title
+  
   type = {
     "auto"            => "Autó",
     "belfolg"         => "Belföld",
@@ -56,146 +65,25 @@ def rssWrite(items, title)
   months  = ["", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dec"]
   timeNow = "#{days[time.wday]}, #{time.day} #{months[time.month]} #{time.year} #{time.hour}:#{time.min}:#{(time.sec.to_s.length == 1 ? '0' + time.sec.to_s : time.sec)} #{dst}"
 
-  version = "2.0"
-  content = RSS::Maker.make(version) do |m|
-    m.channel.title = type[title]
-    m.channel.link = "http://www.hirek.hu/rss.php?page=#{title}"
-    m.channel.description = ""
-    m.items.do_sort = true # sort items by date
-      
-    i = m.items.new_item
-    i.title = "Ruby can parse RSS feeds"
-    i.link = "http://www.rubyrss.com/"
-    i.date = Time.parse("2007/2/11 14:01")
+  rss = RSS::Maker.make("2.0") do |maker|
+    maker.channel.about       = "http://example.com/index.rdf"
+    maker.channel.title       = type[@title]
+    maker.channel.description = "Hirek.hu #{time.year}"
+    maker.channel.link        = "http://www.hirek.hu/rss.php?page=#{title}"
 
-    i = m.items.new_item
-    i.title = "Ruby can create RSS feeds"
-    i.link = "http://www.rubyrss.com/"
-    i.date = Time.now
+  items.each do |thisItem|
+    maker.items.new_item do |item|
+      item.link         = thisItem["link"]
+      item.title        = thisItem["title"]
+      item.description  = thisItem["description"]
+    end
   end
-  puts content
+  end
+  rss
 end
 
-#  xml = Builder::XmlMarkup.new( :target => "", :indent => 2 )
-#  xml.instruct! :xml, :version => "1.0" #:encoding => "US-ASCII"
-
-#  xml.rss do |rssItem|
-#    rssItem.title           type[title]
-#    rssItem.link            "http://www.hirek.hu/rss.php?page=#{title}"
-#    rssItem.description     ""
-#    rssItem.language        "hu-HU"
-#    rssItem.copyright       "Hirek.hu #{time.year}" 
-#    rssItem.managingEditor  "szerkeszto@hirek.hu (Szerkesztő)"
-#    rssItem.webMaster       "webmester@hirek.hu (Webmester)"
-#    rssItem.pubDate         timeNow
-#    rssItem.lastBuildDate   timeNow
-#    rssItem.category        ""
-#    rssItem.generator       "Hirek.hu"
-#    rssItem.docs            "http://blogs.law.harvard.edu/tech/rss"
-#    rssItem.ttl             60
-#    
-#    items.each do |element|
-#      rssItem.item do |el|
-#        el.title        element["title"]
-#        el.link         element["link"]
-#        el.description  element["description"]
-#        el.guid         element["guid"]
-#      end
-#    end
-    
-#    rssItem."atom:link"   (:href => "http://www.hirek.hu/rss.php?page=auto" rel="self" type="application/rss+xml")
-         
-
-  
-#version = "2.0" # ["0.9", "1.0", "2.0"]
-#destination = "test_maker.xml" # local file to write
-
-#content = RSS::Maker.make(version) do |m|
-#  m.channel.title = "Example Ruby RSS feed"
-#  m.channel.link = "http://www.rubyrss.com"
-#  m.channel.description = "Old news (or new olds) at Ruby RSS"
-#  m.items.do_sort = true # sort items by date
-#    
-#  i = m.items.new_item
-#  i.title = "Ruby can parse RSS feeds"
-#  i.link = "http://www.rubyrss.com/"
-#  i.date = Time.parse("2007/2/11 14:01")
-
-#  i = m.items.new_item
-#  i.title = "Ruby can create RSS feeds"
-#  i.link = "http://www.rubyrss.com/"
-#  i.date = Time.now
-#end
-
-#File.open(destination,"w") do |f|
-#f.write(content)
-#end
-  
-  
-#  items.each do |item|
-#    item.each do |key, value|
-#      puts "#{key} is #{value}"
-#    end
-#    
-#  end
-
-#ap items
-
-#<title>Autó</title>
-#      <link>http://www.hirek.hu/rss.php?page=auto</link>
-#      <description></description>
-#      <language>hu-HU</language>
-#      <copyright>Hirek.hu 2011</copyright>
-
-#      <managingEditor>szerkeszto@hirek.hu (Szerkesztő)</managingEditor>
-#      <webMaster>webmester@hirek.hu (Webmester)</webMaster>      
-#      <pubDate>Fri, 23 Sep 2011 12:45:01 +0200</pubDate>
-#      <lastBuildDate>Fri, 23 Sep 2011 12:45:01 +0200</lastBuildDate>
-#      <category></category>
-#      <generator>Hirek.hu</generator>
-#      <docs>http://blogs.law.harvard.edu/tech/rss</docs>
-
-#      <ttl>60</ttl>
-#	<atom:link href="http://www.hirek.hu/rss.php?page=auto" rel="self" type="application/rss+xml" />
-
-#  xml.rss["title"](title)
-  
-
-#  favorites = {
-#    'candy' => 'Neccos', 'novel' => 'Empire of the Sun', 'holiday' => 'Easter'
-#  }
-
-#  xml = Builder::XmlMarkup.new( :target => "", :indent => 2 )
-
-#  xml.instruct! :xml, :version => "1.0"
-
-#  xml.favorites do 
-#    favorites.each do | name, choice |
-#      xml.favorite( choice, :item => name )
-#    end
-#  end
-
-#end
-
-#ap result
-
-#Define URL and Parse Feed
-#feed_url = 'http://index.hu/24ora/rss/'
-#rss = FeedNormalizer::FeedNormalizer.parse open(feed_url)
-
-#Quit if no articles
-#exit unless rss.entries.length > 0
-
-#Read entries
-#rss.entries.each do |entry|
-#	title = entry.title
-#	body = entry.content
-#	authors = entry.authors.join(', ') rescue ''
-#	entry_url = entry.urls.first
-
-
-#  ap "#{title}\n"
-	#Your Logic Here
-#end
-
-#ap rss.entries[0].inspect
+def writeFile(file, content)
+  File.open(file,"w") do |f|
+    f.write(content)
+  end
+end
--- ruby/aggregator.rb
+++ ruby/aggregator.rb
@@ -5,40 +5,52 @@ require 'mysql'
 require 'builder'
 require 'feed-normalizer'
 require 'ap'
+require 'pp'
 
 require 'rss/maker'
 
 require 'IO.rb'
 
-getFeedPagesQuery = "select page_id, page_name, page_xml from pages where page_id > 0"
-getRssFeedsByPage = "select rc.rss_id from page_categories pc inner join rss_categories rc on pc.cat_id = rc.cat_id where pc.page_id="
+rssFeedsQuery = "Select rss_name, cat_id, feed_type, rss_url, id, agencies.agency_id, agency_name, pattern, aux_url, matches, period From feed_cats Left Join rss_feeds On rss_feeds.id=feed_cats.feed_id Left Join agencies On agencies.agency_id=rss_feeds.agency_id Where status=1 And feed_type=1"
+#getFeedPagesQuery = "select page_id, page_name, page_xml from pages where page_id > 0"
+#getRssFeedsByPage = "select rc.rss_id from page_categories pc inner join rss_categories rc on pc.cat_id = rc.cat_id where pc.page_id="
 
+rssFeeds = fetchAll(@db.query(rssFeedsQuery))
 
+rssFeeds.each do |feed|
+  ap feed
+end 
 
 #result = fetchAll(@db.query(getFeedPagesQuery))
 
 #result.each do |res|
 #  query = "Select * From news2 Where rss_id in (#{getRssFeedsByPage}#{res['page_id']}) Order by news2.id DESC Limit 20"
 
-#ap query;
+#  thisResult = fetchAll(@db.query(query))
+
+#  thisResult.each do |thisres|
+#    ap thisres
+#  end
+
+##ap query;
 
 #end
 
-items = [{
-  "title" => "Egymilliárd dollárra perli a Google-t az Oracle - Totalbike.hu",
-  "link"  => "http://www.hirek.hu/?from=auto.xml&amp;page_id=3&amp;news_id=3280109",
-  "description" => "Az Android szabadalomsértései miatt érte kár a céget a vád szerint. Eredetileg hatmilliárdot mondtak, de a bíróság ezt elutasította.",
-  "guid" => "http://www.hirek.hu/?from=auto.xml&amp;page_id=3&amp;news_id=3280109"
-},{
-  "title" => "Egymilliárd dollárra perli a Google-t az Oracle - Totalbike.hu",
-  "link"  => "http://www.hirek.hu/?from=auto.xml&amp;page_id=3&amp;news_id=3280109",
-  "description" => "Az Android szabadalomsértései miatt érte kár a céget a vád szerint. Eredetileg hatmilliárdot mondtak, de a bíróság ezt elutasította.",
-  "guid" => "http://www.hirek.hu/?from=auto.xml&amp;page_id=3&amp;news_id=3280109"
-}]
+#items = [{
+#  "title" => "Egymilliárd dollárra perli a Google-t az Oracle - Totalbike.hu",
+#  "link"  => "http://www.hirek.hu/?from=auto.xml&amp;page_id=3&amp;news_id=3280109",
+#  "description" => "Az Android szabadalomsértései miatt érte kár a céget a vád szerint. Eredetileg hatmilliárdot mondtak, de a bíróság ezt elutasította.",
+#  "guid" => "http://www.hirek.hu/?from=auto.xml&amp;page_id=3&amp;news_id=3280109"
+#},{
+#  "title" => "Egymilliárd dollárra perli a Google-t az Oracle - Totalbike.hu",
+#  "link"  => "http://www.hirek.hu/?from=auto.xml&amp;page_id=3&amp;news_id=3280109",
+#  "description" => "Az Android szabadalomsértései miatt érte kár a céget a vád szerint. Eredetileg hatmilliárdot mondtak, de a bíróság ezt elutasította.",
+#  "guid" => "http://www.hirek.hu/?from=auto.xml&amp;page_id=3&amp;news_id=3280109"
+#}]
 
-ff = rssWrite(items, "auto")
-
-puts ff
+#content = rssWrite(items, "auto")
+#puts content
+#writeFile("test.xml",content)
 
 
 #puts  time.public_methods #"#{time.wday},"
