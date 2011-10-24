2a56ca672580e05017b714dfff16348054280fdf
robThot <robthot@gmail.com>
2011-10-24 21:41:00 +0200

    io.rb transmission oncomplete script added

---
+++ transmission-trigger.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+downloads=/home/vvv/Downloads
+movies=/home/vvv/Videos
+log=$downloads/log.txt
+TR_DOWNLOADED_PATH="$TR_TORRENT_DIR/$TR_TORRENT_NAME"
+targetdir=$movies/$TR_TORRENT_NAME/
+cd $TR_DOWNLOADED_PATH
+mkdir $targetdir
+echo $TR_DOWNLOADED_PATH >> $log
+echo $targetdir >> $log
+for i in $(find -name *.rar);
+do
+  unrar e $i;
+  echo $i >> $log
+done;
+for i in $(find -name *.avi);
+do
+  mv $i $targetdir;
+  echo $i >> $log
+done;
+for i in $(find -name *.mkv);
+do
+  mv $i $targetdir;
+  echo $i >> $log
+done;
+
+echo "-------------------------------" >> $log
+echo "===============================" >> $log
+echo "-------------------------------" >> $log
