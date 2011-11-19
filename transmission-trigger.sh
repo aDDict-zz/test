7cbc8b9e7a00dbe1a74ae04c10b5e2c750523fa6
robThot <robthot@gmail.com>
2011-11-19 17:58:45 +0100

    transmission script refact

--- transmission-trigger.sh
+++ transmission-trigger.sh
@@ -4,20 +4,22 @@ movies="/home/vvv/Videos"
 log="$downloads/log.txt"
 TR_DOWNLOADED_PATH="$TR_TORRENT_DIR/$TR_TORRENT_NAME"
 targetdir="$movies/$TR_TORRENT_NAME/"
+target="$movies/$TR_TORRENT_NAME/$TR_TORRENT_NAME"
 
 echo "===============================" >> $log
 echo "$TR_TORRENT_DIR" >> $log
 echo "$TR_TORRENT_NAME" >> $log
 echo "$targetdir" >> $log
 echo "$TR_DOWNLOADED_PATH" >> $log
+echo "$target" >> $log
 echo "===============================" >> $log
 
 mkdir "$targetdir";
 
 find "$TR_DOWNLOADED_PATH" -name \*.rar -exec unrar e {} \;
-find "$TR_DOWNLOADED_PATH" -name \*.avi -exec mv {} "$targetdir" \;
-find "$TR_DOWNLOADED_PATH" -name \*.iso -exec mv {} "$targetdir" \;
-find "$TR_DOWNLOADED_PATH" -name \*.mkv -exec mv {} "$targetdir" \;
+find "$TR_DOWNLOADED_PATH" -name \*.avi -exec mv {} "$target.avi" \;
+find "$TR_DOWNLOADED_PATH" -name \*.iso -exec mv {} "$targetdir.iso" \;
+find "$TR_DOWNLOADED_PATH" -name \*.mkv -exec mv {} "$targetdir.mkv" \;
 
 # find -name \*.rar -exec unrar e {} \;
 
